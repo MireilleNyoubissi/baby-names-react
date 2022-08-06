bf01fe38f7798ca8286205ba7c6f29cfdf137515
MireilleNyoubissi <nmireillemat@yahoo.fr>
2022-08-06 12:43:39 +0100

    updated babyNamesList

--- src/BabyNamesList.js
+++ src/BabyNamesList.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from 'react';
 
+function BabyNamesList(props) {
+  // const [babyName, setBabyName] = useState("");
 
-function BabyNamesList(props){
-  
-    return (
+  const [favorites, setFavorites] = useState([]);
+
+  const addFavorites = (babyName) => {
+    setFavorites(favorites.concat([babyName + '']));
+  };
+
+  return (
+    <>
+      <div className="div-wrap">
+        <h4>Favorites: </h4>
+        <p>
+          {favorites.map((favorite, i) => (
+            <li key={i}>{favorite + ','}</li>
+          ))}
+        </p>
+      </div>
+      <hr />
       <ul>
-        {props.data.map((babyName) => (
-          <li key={babyName.id}>
-            {babyName.name}
-          </li>
+        {props.data.map((babyName, index) => (
+          <>
+            {babyName.sex === 'm' ? (
+              <li
+                onClick={(e) => addFavorites(babyName.name)}
+                className="genderFormatBoy"
+                key={babyName.id}
+              >
+                {babyName.name}
+              </li>
+            ) : (
+              <li
+                onClick={(e) => addFavorites(babyName.name)}
+                className="genderFormatGirl"
+                key={babyName.id}
+              >
+                {babyName.name}
+              </li>
+            )}
+          </>
         ))}
       </ul>
-    );
+    </>
+  );
 }
-
 
 export default BabyNamesList;
